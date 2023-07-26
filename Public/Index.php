370436a486d46db492a9fe933c2388c9d762cef2
Jeoffrey80 <meyxana80@gmail.com>
2023-07-26 10:54:31 +0200

    .

--- Public/Index.php
+++ Public/Index.php
@@ -1,11 +1,10 @@
-
 <head>
 </head>
     <body>
     
-    <?php
-    include 'header.php';
-    ?>
+                <?php
+                include 'header.php';
+                ?>
                 <div class="main-content">
 
                 </div>   
